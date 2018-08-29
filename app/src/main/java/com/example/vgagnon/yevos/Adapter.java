9137e392dd321eeba3a77d8ff6a654bbdfbdf7d1
Vincent <vincent.gagnon@gmail.com>
2018-08-29 11:20:48 -0400

    Test multitech - non fonctionnel

---
+++ app/src/main/java/com/example/vgagnon/yevos/Adapter.java
@@ -0,0 +1,71 @@
+package com.example.vgagnon.yevos;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+/**
+ * Created by Parsania Hardik on 26-Jun-17.
+ */
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+    private LayoutInflater inflater;
+    private ArrayList<Model> imageModelArrayList;
+
+
+    public Adapter(Context ctx, ArrayList<Model> imageModelArrayList){
+
+        inflater = LayoutInflater.from(ctx);
+        this.imageModelArrayList = imageModelArrayList;
+    }
+
+    public void removeItem(int position) {
+        imageModelArrayList.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, imageModelArrayList.size());
+    }
+    public void restoreItem(Model model, int position) {
+        imageModelArrayList.add(position, model);
+        // notify item added by position
+        notifyItemInserted(position);
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.rv_item, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        holder.time.setText(imageModelArrayList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageModelArrayList.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView time;
+
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            time = (TextView) itemView.findViewById(R.id.tv);
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/vgagnon/yevos/Multitech_Call_In.java
+++ app/src/main/java/com/example/vgagnon/yevos/Multitech_Call_In.java
@@ -1,73 +1,144 @@
 package com.example.vgagnon.yevos;
 
-import android.content.Intent;
-import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
-
 import java.util.ArrayList;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.RectF;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.graphics.Canvas;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+
 
 public class Multitech_Call_In extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView recyclerView;
-    private ContactsAdapter mAdapter;
-    ArrayList<Contact> contacts;
+    private ArrayList<Model> imageModelArrayList;
+    private Adapter adapter;
+    private Paint p = new Paint();
+
+    private String[] myImageNameList = new String[]{"Benz", "Bike",
+            "Car","Carrera"
+            ,"Ferrari","Harly",
+            "Lamborghini","Silver"};
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multitech__call__in);
-        // Lookup the recyclerview in activity layout
-        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
 
+        imageModelArrayList = populateList();
 
+        adapter = new Adapter(this,imageModelArrayList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
 
-        // Initialize contacts
-        contacts = Contact.createContactsList(20);
-        // Create adapter passing in the sample user data
-        ContactsAdapter adapter = new ContactsAdapter(contacts);
-        // Attach the adapter to the recyclerview to populate items
-        rvContacts.setAdapter(adapter);
-        // Set layout manager to position the items
-        rvContacts.setLayoutManager(new LinearLayoutManager(this));
-        // That's all!
-        // attaching the touch helper to recycler view
-        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(rvContacts);
+        enableSwipe();
     }
-    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
 
+    private void enableSwipe(){
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
 
-        @Override
-        public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-            // view the background view
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+
+                if (direction == ItemTouchHelper.LEFT){
+                    final Model deletedModel = imageModelArrayList.get(position);
+                    final int deletedPosition = position;
+                    adapter.removeItem(position);
+                    // showing snack bar with Undo option
+                    Snackbar snackbar = Snackbar.make(getWindow().getDecorView().getRootView(), " removed from Recyclerview!", Snackbar.LENGTH_LONG);
+                    snackbar.setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // undo is selected, restore the deleted item
+                            adapter.restoreItem(deletedModel, deletedPosition);
+                        }
+                    });
+                    snackbar.setActionTextColor(Color.YELLOW);
+                    snackbar.show();
+                } else {
+                    final Model deletedModel = imageModelArrayList.get(position);
+                    final int deletedPosition = position;
+                    adapter.removeItem(position);
+                    // showing snack bar with Undo option
+                    Snackbar snackbar = Snackbar.make(getWindow().getDecorView().getRootView(), " removed from Recyclerview!", Snackbar.LENGTH_LONG);
+                    snackbar.setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            // undo is selected, restore the deleted item
+                            adapter.restoreItem(deletedModel, deletedPosition);
+                        }
+                    });
+                    snackbar.setActionTextColor(Color.YELLOW);
+                    snackbar.show();
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                Bitmap icon;
+                if(actionState == ItemTouchHelper.ACTION_STATE_SWIPE){
+
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+
+                    if(dX > 0){
+                        p.setColor(Color.parseColor("#388E3C"));
+                        RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX,(float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.delete);
+                        RectF icon_dest = new RectF((float) itemView.getLeft() + width ,(float) itemView.getTop() + width,(float) itemView.getLeft()+ 2*width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    } else {
+                        p.setColor(Color.parseColor("#D32F2F"));
+                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(),(float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.delete);
+                        RectF icon_dest = new RectF((float) itemView.getRight() - 2*width ,(float) itemView.getTop() + width,(float) itemView.getRight() - width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    private ArrayList<Model> populateList(){
+
+        ArrayList<Model> list = new ArrayList<>();
+
+        for(int i = 0; i < 8; i++){
+            Model imageModel = new Model();
+            imageModel.setName(myImageNameList[i]);
+            list.add(imageModel);
         }
-    };
-    @Override
-    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-        return false;
+
+        return list;
     }
 
-    @Override
-    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
-        // Row is swiped from recycler view
-        // remove it from adapter
-        if (viewHolder instanceof ContactsAdapter.ViewHolder) {
 
-            // remove the item from recycler view
-            mAdapter.removeItem(viewHolder.getAdapterPosition());
-
-            // showing snack bar with Undo option
-
-        }
-    }
 
 }
 
